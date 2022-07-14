585a0e2b8872f237e5b447ea64c31d202c6fa775
Cezar Augusto <cezar.ags@icloud.com>
2022-07-13 21:59:49 -0300

    First Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,20 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import NotificationButton from './components/NotificationButton'
 
 function App() {
  return (
+  <>
     <h1>Olá, mundo</h1>
+    <NotificationButton/>
+    <NotificationButton/>
+    <NotificationButton/>
+    <NotificationButton/>
+    <NotificationButton/>
+    </>  
  )
 }
+
 
 export default App
