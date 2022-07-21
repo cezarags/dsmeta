147e24b3a3121b9af121b058b17aba3fa9e94649
Cezar Augusto <cezar.ags@icloud.com>
2022-07-21 18:29:30 -0300

    added Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,24 @@
+import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/img/Notificationicon.svg'
+import { BASE_URL } from '../../utils/request';
 import './styles.css'
 
-function NotificationButton() {
+
+   type Props = {
+      saleId: number;
+   }
+
+   function hancdleClick(saleId: number){
+      axios(`${BASE_URL}/sales/${saleId}/notification`)
+      .then(response => {
+         toast.info("SMS enviado com sucesso!")
+      })
+   }
+
+function NotificationButton({saleId}: Props) {
  return (
-    <div className="dsmeta-red-btn">
+    <div className="dsmeta-red-btn" onClick={() => hancdleClick(saleId)}>
     <img src={icon} alt="Notificar" />
     </div>
  )
